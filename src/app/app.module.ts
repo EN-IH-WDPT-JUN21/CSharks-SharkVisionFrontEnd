716e6afd0b038a7be44aaa578b49ebc0712673f5
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-11-09 14:47:07 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserPlaylistComponent } from './user-playlist/user-playlist.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserProfileComponent,
+    UserPlaylistComponent,
+    MovieSearchComponent,
+    MovieDetailComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
