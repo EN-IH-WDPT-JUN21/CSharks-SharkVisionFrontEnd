ce856cfff7357dd80d76b905b3f6ce900c0065df
Jegor Chomczuk <jegor.chomczuk@gmail.com>
2021-11-17 17:34:59 +0100

    BE API integration

---
+++ src/app/services/playlist.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Playlist } from '../models/playlist.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaylistService {
+  readonly baseUrl:string = "http://localhost:8000/movie-app/playlists/";
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  // Get all Playlists
+  getPlaylists() : Observable<any> {
+    return this.http.get<Playlist[]>(this.baseUrl + 'all');
+  }
+
+  // Get a Playlists by id
+  getPlaylist(id: number) : Observable<any> {
+    return this.http.get<Playlist>(this.baseUrl + id);
+  }
+
+  // Get a Playlist by User id
+  getPlaylistByUserId(userId: number) : Observable<any> {
+    return this.http.get<Playlist[]>(this.baseUrl + '/user/' + userId);
+  }
+
+  // Delete a Playlist
+  deletePlaylist(id: number) : Observable<any> {
+    return this.http.delete(this.baseUrl + id + '/delete');
+  }
+
+  // Add a Movie to a Playlist
+  addMovie(playlistId:number, titleId: number) : Observable<any> {
+    return this.http.put(this.baseUrl + playlistId + '/add/' + titleId, {});
+  }
+
+  // Delete a Movie from a Playlist 
+  removeMovie(playlistId:number, titleId: number) : Observable<any> {
+    return this.http.put(this.baseUrl + playlistId + '/remove/' + titleId, {});
+  }
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Playlist } from '../models/playlist.model';
 
 @Injectable({
   providedIn: 'root'
@@ -30,5 +32,30 @@ export class UserService {
     const body = { "emailAddress": email };
     return this.http.post<any>(this.baseUrl + "/validate/email", body);
   }
+
+    // Get all Users
+    getUsers() : Observable<any> {
+      return this.http.get<User[]>(this.baseUrl + 'all');
+    }
+    
+    // Get User by id
+    getUser(id: number) : Observable<any> {
+      return this.http.get<User>(this.baseUrl + id);
+    }
+  
+    // Register a User
+    registerUser(user: Object): Observable<Object> {
+      return this.http.post(this.baseUrl, user);
+    }
+  
+    // Update User
+    setUser(id: number, value: any): Observable<Object> {
+      return this.http.put(this.baseUrl + id + '/set', value);
+    }
+  
+    // Create Playlist
+    createPlaylist(id: number, playlist: Playlist): Observable<Object> {
+      return this.http.post(this.baseUrl + id + '/createPlaylist', playlist);
+    }
 
 }
