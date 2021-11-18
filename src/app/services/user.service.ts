f063eab867c557f13a9b8620ab5433431e2e6df7
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-11-18 20:05:36 +0000

    Favicon updated, and "add to playlist" button

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -20,7 +20,6 @@ export class UserService {
     return this.http.post<any>(this.baseUrl + "/register", body);
   }
 
-
   checkUsernameExists(username: string): Observable<boolean> {
     const body = { "username": username };
     return this.http.post<any>(this.baseUrl + "/validate/username", body);
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../services/user.service';
 import { AuthService } from './../services/auth.service';
 import { MovieDetail } from './../models/movieDetail.model';
 import { MovieService } from './../services/movie.service';
@@ -12,35 +13,36 @@ import { PopularMovieResponse } from '../models/popularMovie.model';
 export class WelcomePageComponent implements OnInit {
   isLoggedIn = false;
   popularMovies: MovieDetail[];
-  randomMovie:MovieDetail;
-  showDetail:boolean;
+  randomMovie: MovieDetail;
+  showDetail: boolean;
 
-  constructor(private auth: AuthService, private movieService: MovieService) {
+  currentUser: number;
+  userPlaylists: string[];
+  showPlaylists: boolean;
+
+  constructor(private auth: AuthService, private movieService: MovieService, private userService: UserService) {
     this.popularMovies = [];
-    this.randomMovie = new MovieDetail('','','','','','','','','','');
-    this.showDetail = false; 
-   }
+    this.randomMovie = new MovieDetail('', '', '', '', '', '', '', '', '', '');
+    this.showDetail = false;
+    this.userPlaylists = [];
+    this.currentUser = 0;
+    this.showPlaylists = false;
+  }
 
   ngOnInit(): void {
     if (this.auth.isLoggedIn()) this.isLoggedIn = true;
-    
+
     this.popularMovies = [];
-    this.movieService.getPopularMovies().subscribe(result => {
-        const popMovieResponse: PopularMovieResponse = result;
-        var i = 0;
-        for ( let movie of popMovieResponse.items) {
-          const id:string = popMovieResponse.items[i].id;
-          const title:string = popMovieResponse.items[i].fullTitle;
-          const image:string = popMovieResponse.items[i].image;
-          const imDbRating:string = popMovieResponse.items[i].imDbRating;
-          let movie = new MovieDetail(id,title,'',image,'','','','','',imDbRating);
-          this.popularMovies.push(movie);
-          i++;
-        }}
-    )
+
+    // this.generatePopMovies();
+
+    this.getCurrentUser();
+    this.getUserPlaylists(this.currentUser);
+
+    this.userPlaylists = ['1', '2', '3', '1', '2', '3'];
   }
 
-  randomMovieGenerator() :void {
+  randomMovieGenerator(): void {
     var index = Math.floor(Math.random() * (99 + 1));
     this.movieService.getMovieById(this.popularMovies[index].id).subscribe(
       result => {
@@ -50,13 +52,44 @@ export class WelcomePageComponent implements OnInit {
     this.showDetail = true;
   }
 
-  addToPlayList():void {
+  addToPlayList(): void {
 
   }
 
-  back():void {
+  back(): void {
     this.showDetail = false;
+    this.showPlaylists = false;
   }
 
+  getCurrentUser(): void {
+    this.userService.getCurrentUser().subscribe(result => {
+      this.currentUser = result;
+    })
+  }
 
+  generatePopMovies(): void {
+    this.movieService.getPopularMovies().subscribe(result => {
+      const popMovieResponse: PopularMovieResponse = result;
+      var i = 0;
+      for (let movie of popMovieResponse.items) {
+        const id: string = popMovieResponse.items[i].id;
+        const title: string = popMovieResponse.items[i].fullTitle;
+        const image: string = popMovieResponse.items[i].image;
+        const imDbRating: string = popMovieResponse.items[i].imDbRating;
+        let movie = new MovieDetail(id, title, '', image, '', '', '', '', '', imDbRating);
+        this.popularMovies.push(movie);
+        i++;
+      }
+    }
+    )
+  }
+
+  getUserPlaylists(userId: number): void {
+
+  }
+
+  addToPlaylist() {
+    this.showPlaylists = false;
+  }
 }
+
